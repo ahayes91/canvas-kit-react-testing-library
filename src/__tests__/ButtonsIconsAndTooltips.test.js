d89785bafec8cf6f1c02358d212e772d5a05d977
Aislinn Hayes <aislinn.hayes@workday.com>
2022-12-01 12:50:59 +0000

    Remvoe user-event and update React and testing-library packages to latest

--- src/__tests__/ButtonsIconsAndTooltips.test.js
+++ src/__tests__/ButtonsIconsAndTooltips.test.js
@@ -1,7 +1,6 @@
 import "@testing-library/jest-dom/extend-expect";
 import React from "react";
-import { render, waitFor } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
+import { render } from "@testing-library/react";
 import { ButtonWithTooltipAndIcon } from "../ButtonWithTooltipAndIcon";
 import { ButtonWithTooltipNoIcon } from "../ButtonWithTooltipNoIcon";
 import { ButtonWithIcon } from "../ButtonWithIcon";
@@ -9,61 +8,46 @@ import { ButtonWithIconWithIncorrectPosition } from "../ButtonWithIconWithIncorr
 import { ButtonWithIconWithCorrectPosition } from "../ButtonWithIconWithCorrectPosition";
 
 test("Button with tooltip and icon fails", async () => {
-  const { getByRole, queryByText } = render(
-    <ButtonWithTooltipAndIcon handleClick={() => {}} label="With icon" />
+  const handleClick = jest.fn();
+  const { getByRole } = render(
+    <ButtonWithTooltipAndIcon handleClick={handleClick} label="With icon" />
   );
   const openModalButton = getByRole("button", { name: "With icon" }); // This fails
   expect(openModalButton).toBeInTheDocument();
-  userEvent.click(openModalButton);
-  await waitFor(() => {
-    expect(queryByText("Whatever")).not.toBeInTheDocument();
-  });
 });
 
 test("Button with tooltip and no icon passes", async () => {
-  const { getByRole, queryByText } = render(
-    <ButtonWithTooltipNoIcon handleClick={() => {}} label="Without icon" />
+  const handleClick = jest.fn();
+  const { getByRole } = render(
+    <ButtonWithTooltipNoIcon handleClick={handleClick} label="Without icon" />
   );
   const openModalButton = getByRole("button", { name: "Without icon" });
   expect(openModalButton).toBeInTheDocument();
-  userEvent.click(openModalButton);
-  await waitFor(() => {
-    expect(queryByText("Whatever")).not.toBeInTheDocument();
-  });
 });
 
 test("Button with icon and no tooltip or position fails", async () => {
-  const { getByRole, queryByText } = render(
-    <ButtonWithIcon handleClick={() => {}} label="With icon" />
+  const handleClick = jest.fn();
+  const { getByRole } = render(
+    <ButtonWithIcon handleClick={handleClick} label="With icon" />
   );
   const openModalButton = getByRole("button", { name: "With icon" }); // This fails
   expect(openModalButton).toBeInTheDocument();
-  userEvent.click(openModalButton);
-  await waitFor(() => {
-    expect(queryByText("Whatever")).not.toBeInTheDocument();
-  });
 });
 
 test("Button with icon and incorrect position and no tooltip passes", async () => {
-  const { getByRole, queryByText } = render(
-    <ButtonWithIconWithIncorrectPosition handleClick={() => {}} label="With icon" />
+  const handleClick = jest.fn();
+  const { getByRole } = render(
+    <ButtonWithIconWithIncorrectPosition handleClick={handleClick} label="With icon" />
   );
   const openModalButton = getByRole("button", { name: "With icon" }); // This fails
   expect(openModalButton).toBeInTheDocument();
-  userEvent.click(openModalButton);
-  await waitFor(() => {
-    expect(queryByText("Whatever")).not.toBeInTheDocument();
-  });
 });
 
 test("Button with icon and correct position and no tooltip fails", async () => {
-  const { getByRole, queryByText } = render(
-    <ButtonWithIconWithCorrectPosition handleClick={() => {}} label="With icon" />
+  const handleClick = jest.fn();
+  const { getByRole } = render(
+    <ButtonWithIconWithCorrectPosition handleClick={handleClick} label="With icon" />
   );
   const openModalButton = getByRole("button", { name: "With icon" }); // This fails
   expect(openModalButton).toBeInTheDocument();
-  userEvent.click(openModalButton);
-  await waitFor(() => {
-    expect(queryByText("Whatever")).not.toBeInTheDocument();
-  });
 });
